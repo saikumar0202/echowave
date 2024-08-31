487e42563e0965778dc9055469cabddee4681c95
saikumar0202 <saikumaranapuram@gmail.com>
2024-08-31 22:13:31 +0530

    Reduct implmentationa and initial styles setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import store from './redux/store';
 import { Sidebar } from "./components/Sidebar";
 import { Discover } from "./components/Discover";
 import { Player } from "./components/Player";
 import { Track } from "./components/Track";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-      <main className='main-container'>
-        <Discover />
-        <Player />
-        <Track />
-      </main>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Sidebar />
+        <main className='main-container'>
+          <Discover />
+          <Player />
+          <Track />
+        </main>
+      </div>
+    </Provider>
   );
 }
 
