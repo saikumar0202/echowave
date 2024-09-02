f2da9833c4f52b73ef3f9a9e3db5b2898d490d7c
Koti Reddy <kotireddykolli1998@gmail.com>
2024-09-02 22:21:37 +0530

    side bar init changes

--- src/components/Track/index.tsx
+++ src/components/Track/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { SongList } from "../SongList";
+import {songs} from "../Discover/index";
+import "./styles.scss"
 export const Track = () => {
     return (
-        <article className='track-section'>Tracker</article>
+        <article className='track-section'>
+            <h2 className="track-section__title">Track List</h2>
+            <div className="track-section__actions">
+                <button className="control-button control-button--shuffle"><i className="fa-solid fa-shuffle"></i></button>
+                <button className="control-button control-button--repeat"><i className="fa-solid fa-repeat"></i></button>
+            </div>
+            <div>
+                <SongList songs={songs} type="PLAIN"/>
+            </div>
+        </article>
     );
 };
